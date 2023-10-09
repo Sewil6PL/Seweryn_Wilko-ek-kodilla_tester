db3a8b7dc4927824e0c9e4e38853b3ec47dc3329
Seweryn Wilkołek <wilkolak82@gmail.com>
2023-10-09 18:52:58 +0200

    HelloWorld imię nazwisko

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from FirstClass!");
+        System.out.println("Mam na imię Seweryn");
+        System.out.println("Moje nazwisko to Wilkołek");
+        System.out.println("Pierwsza litera nazwisko to W");
+    }
+}
