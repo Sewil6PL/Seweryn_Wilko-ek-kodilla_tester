2313b852106e6cc16724010ec9b89eb513bc3dfb
Seweryn Wilkołek <wilkolak82@gmail.com>
2023-10-15 17:35:24 +0200

    kodilla-course

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myJob = "Magazynier";
+        int yearExperience = 2;
+        int yearOfEmployment = 2021;
+        double tonsPerDay = 3.20; // Ile dziennie przerzucam mebli w tonach
+        char employeeClass = 'A';
+        boolean isGoodJob = false;
+        System.out.println(myJob);
+        System.out.println(yearExperience);
+        System.out.println(yearOfEmployment);
+        System.out.println(tonsPerDay);
+        System.out.println(employeeClass);
+        System.out.println(isGoodJob);
+    }
+}
+
