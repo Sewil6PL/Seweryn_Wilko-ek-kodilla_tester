88d358d8c52a680b56c7619c83618854dee21f5f
Seweryn Wilkołek <wilkolak82@gmail.com>
2023-11-12 21:34:05 +0100

    kodilla-cours

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -7,13 +7,24 @@ public class Application {
         this.age = age;
         this.height = height;
     }
+    public String getName(){
+        return name;
+    }
+    public double getAge(){
+        return age;
+    }
+    public double getHeight(){
+        return height;
+    }
+
     public void getApplication(){
-        if (name != null) {
-            if (age > 30 && height > 160) {
+        if (this.name != null) {
+            if (this.age > 30 && this.height > 160) {
                 System.out.println("User is older than 30 and taller than 160cm");
             } else {
                 System.out.println("User is 30 (or younger) or 160cm (or shorter)");
             }
         }
     }
+    
 }
