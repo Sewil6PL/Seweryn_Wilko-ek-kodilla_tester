1656129ea4165916b05e3648e69be8e9a1ed0ea0
Seweryn Wilkołek <wilkolak82@gmail.com>
2023-12-11 14:50:27 +0100

    kodilla-cours

--- kodilla-stream/src/test/java/com/kodilla/stream/UsersManagerTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/UsersManagerTest.java
@@ -1,14 +1,15 @@
 package com.kodilla.stream;
 
+
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UsersManagerTest {
-
 
     @Test
     public void testFilterChemistGroupUsernames() {
@@ -20,9 +21,46 @@ class UsersManagerTest {
         List<String> expectedUsernames = Arrays.asList("Walter White", "Gale Boetticher");
         assertEquals(expectedUsernames, resultUsernames);
     }
-    //int ageThreshold = 40;
-    //List<User> resultUsers = UsersManager.filterUsersOlderThan(null, ageThreshold);
-       // private assertEquals(2,resultUsers.size());
-        //for (User user ; resultUsers) {
-        //ssertTrue(user.getAge() > ageThreshold);
+
+    @Test
+    public void testFilterUsersOlderThan() {
+        // given
+        int ageThreshold = 30;
+        List<User> users = UsersRepository.getUsersList();
+
+        // when
+        List<User> resultUsers = UsersManager.filterUsersOlderThan(users, ageThreshold);
+
+        // then
+        assertEquals(5, resultUsers.size());
+        for (User user : resultUsers) {
+            assertTrue(user.getAge() > ageThreshold);
+        }
     }
+
+    @Test
+    public void testFilterUsersOlderThanWithEmptyList() {
+        // given
+        int ageThreshold = 30;
+        List<User> users = List.of(); // empty list
+
+        // when
+        List<User> resultUsers = UsersManager.filterUsersOlderThan(users, ageThreshold);
+
+        // then
+        assertEquals(0, resultUsers.size());
+    }
+
+    @Test
+    public void testFilterUsersOlderThanWithNoMatchingUsers() {
+        // given
+        int ageThreshold = 60;
+        List<User> users = UsersRepository.getUsersList();
+
+        // when
+        List<User> resultUsers = UsersManager.filterUsersOlderThan(users, ageThreshold);
+
+        // then
+        assertEquals(0, resultUsers.size());
+    }
+}
