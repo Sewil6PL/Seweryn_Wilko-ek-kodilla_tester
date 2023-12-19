c022847cb56bf7181a364f2cdfe754db3f3c6da0
Seweryn Wilkołek <wilkolak82@gmail.com>
2023-12-19 18:47:12 +0100

    kodilla-cours

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/provideTestData.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/provideTestData.java
@@ -5,16 +5,16 @@ import java.util.stream.Stream;
 public class provideTestData {
     static Stream<Object[]> provideTestData() {
         return Stream.of(
-                new Object[]{new Person(1.70, 50), "Very severely underweight"},
-                new Object[]{new Person(1.70, 55), "Severely underweight"},
-                new Object[]{new Person(1.70, 60), "Underweight"},
+                new Object[]{new Person(1.70, 50.0), "Underweight"},
+                new Object[]{new Person(1.70, 55.5), "Normal (healthy weight)"},
+                new Object[]{new Person(1.70, 60.0), "Normal (healthy weight)"},
                 new Object[]{new Person(1.70, 70), "Normal (healthy weight)"},
                 new Object[]{new Person(1.70, 80), "Overweight"},
                 new Object[]{new Person(1.70, 95), "Obese Class I (Moderately obese)"},
                 new Object[]{new Person(1.70, 110), "Obese Class II (Severely obese)"},
                 new Object[]{new Person(1.70, 130), "Obese Class III (Very severely obese)"},
-                new Object[]{new Person(1.70, 150), "Obese Class IV (Morbidly Obese)"},
-                new Object[]{new Person(1.70, 180), "Obese Class V (Super Obese)"},
+                new Object[]{new Person(1.70, 150), "Obese Class V (Super Obese)"},
+                new Object[]{new Person(1.70, 180), "Obese Class VI (Hyper Obese)"},
                 new Object[]{new Person(1.70, 200), "Obese Class VI (Hyper Obese)"}
         );
     }
