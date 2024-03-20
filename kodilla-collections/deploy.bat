025be118a6dc8ffc9c7fa3516a2774f3cb109e4d
Seweryn Wilkołek <wilkolak82@gmail.com>
2024-03-20 12:18:02 +0100

    kodilla-cours

--- kodilla-collections/deploy.bat
+++ kodilla-collections/deploy.bat
@@ -1,14 +1,14 @@
-@echo off
+rem @echo off
 REM Usuń wszystkie pliki *.jar z katalogu build/libs
-del /Q "build\libs\*.jar"
+del /Q build\libs\*.jar
 
 REM Wywołaj gradlew build
-gradlew build
+call gradlew.bat build
 
 REM Sprawdź czy istnieje plik *.jar w katalogu build/libs
 if exist "build\libs\*.jar" (
     REM Skopiuj plik *.jar do katalogu project
-    copy "build\libs\*.jar" "C:\project"
+    copy build\libs\*.jar C:\project
 ) else (
     REM Wyświetl komunikat o błędzie kompilacji
     echo Błąd: Nie udało się skompilować projektu.
---
+++ kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/BrowserTest.java
@@ -0,0 +1,53 @@
+package com.kodilla.testcontainers;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.DefaultRecordingFileFactory;
+import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
+
+public class BrowserTest {
+
+    public BrowserWebDriverContainer chromeContainer;
+    @Rule
+    public BrowserWebDriverContainer firefox =
+            new BrowserWebDriverContainer()
+                    .withCapabilities(new FirefoxOptions())
+                    .withRecordingMode(RECORD_ALL, new File("./build/"))
+                    .withRecordingFileFactory(new DefaultRecordingFileFactory());
+
+    @Before
+    public void setUp() {
+        chromeContainer = new BrowserWebDriverContainer()
+                .withCapabilities(new ChromeOptions());
+        chromeContainer.start();
+    }
+
+    @Test
+    public void chromeTest() throws IOException {
+        RemoteWebDriver driver = chromeContainer.getWebDriver();
+        driver.get("http://allegro.pl");
+
+        File screenshot = driver.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot,
+                new File("./build/screenshots/" + screenshot.getName()));
+    }
+
+    @After
+    public void tearDown() {
+        chromeContainer.stop();
+    }
+
+}
